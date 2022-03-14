3ece9cbd567bae796d9d9d602e33d3cc8153aaf7
Joakim Teixeira <setokim1@hotmail.com>
2022-03-13 22:04:11 -0300

    Add initial files

---
+++ custom.d.ts
@@ -0,0 +1,9 @@
+declare module '*.svg?inline' {
+  const ReactComponent: any;
+  export { ReactComponent };
+}
+
+declare module '*.svg' {
+  const ReactComponent: any;
+  export { ReactComponent };
+}
