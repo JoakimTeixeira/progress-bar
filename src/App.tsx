aac4f9e94adda1875af51d3ede1e4a0e73e74038
Joakim Teixeira <setokim1@hotmail.com>
2022-03-17 10:45:40 -0300

    Change App component to arrow function

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 import { Accordion } from './components/Accordion';
 import './App.css';
 
-function App() {
-  return (
-    <div className="container">
-      <div className="group-container">
-        <h1 className="group-title">Grouped Tasks</h1>
-        <main>
-          <Accordion />
-        </main>
-      </div>
+const App = () => (
+  <div className="container">
+    <div className="group-container">
+      <h1 className="group-title">Grouped Tasks</h1>
+      <main>
+        <Accordion />
+      </main>
     </div>
-  );
-}
+  </div>
+);
 
 export default App;
