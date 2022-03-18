60f9330cf659ac6c0ab3f56ffe59123fba2f1439
Joakim Teixeira <setokim1@hotmail.com>
2022-03-18 20:57:29 -0300

    Add Progress Bar component structure without logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { ProgressBar } from 'components/ProgressBar';
 import './App.css';
 import { Accordion } from './components/Accordion';
 
 const App = () => (
   <div className="container">
     <div className="group-container">
-      <h1 className="group-title">Grouped Tasks</h1>
+      <header className="group-header">
+        <h1 className="group-title">Grouped Tasks</h1>
+        <ProgressBar />
+      </header>
       <main>
         <Accordion />
       </main>
---
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,7 @@
+import './ProgressBar.css';
+
+export const ProgressBar = () => (
+  <div className="progress-bar-container">
+    <div className="progress-bar-text">100%</div>
+  </div>
+);
