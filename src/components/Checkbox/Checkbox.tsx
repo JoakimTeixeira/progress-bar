e103d2de6998095a372b524d26ace1e02a6842b1
Joakim Teixeira <setokim1@hotmail.com>
2022-03-18 11:03:36 -0300

    Implement generic function to Checkbox component

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,14 +1,21 @@
-import React, { FC } from 'react';
+import React, { FC, useRef } from 'react';
 import { ITaskData } from 'interfaces/groups';
+import { getArrayWithId } from 'utils/formatData';
 import './Checkbox.css';
 
-export const Checkbox: FC<{ tasks: ITaskData[] }> = ({ tasks }) => (
-  <>
-    {tasks.map((task) => (
-      <div key={task.description} className="checkbox-item">
-        <input type="checkbox" id={task.description} checked={task.checked} />{' '}
-        {task.description ?? task.name}
-      </div>
-    ))}
-  </>
-);
+export const Checkbox: FC<{ tasks: ITaskData[] }> = ({ tasks }) => {
+  const formattedTasks = useRef<ITaskData[]>();
+
+  formattedTasks.current = getArrayWithId(tasks);
+
+  return (
+    <>
+      {formattedTasks.current.map((task) => (
+        <div key={task.id} className="checkbox-item">
+          <input type="checkbox" id={task.description} defaultChecked={task.checked} />
+          {task.description ?? task.name}
+        </div>
+      ))}
+    </>
+  );
+};
--- src/interfaces/groups.ts
+++ src/interfaces/groups.ts
@@ -1,4 +1,5 @@
 export interface ITaskData {
+  id: string;
   name?: string;
   description?: string;
   value: number;
