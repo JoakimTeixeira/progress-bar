358e8136a5e4c048ea74161bad0d239e6273a26a
Joakim Teixeira <setokim1@hotmail.com>
2022-03-19 00:37:19 -0300

    Add global context for groups data

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import { ProgressBar } from 'components/ProgressBar';
+import { GroupsContextProvider } from 'contexts/GroupsContext';
 import './App.css';
 import { Accordion } from './components/Accordion';
 
 const App = () => (
-  <div className="container">
-    <div className="group-container">
-      <header className="group-header">
-        <h1 className="group-title">Grouped Tasks</h1>
-        <ProgressBar />
-      </header>
-      <main>
-        <Accordion />
-      </main>
+  <GroupsContextProvider>
+    <div className="container">
+      <div className="group-container">
+        <header className="group-header">
+          <h1 className="group-title">Grouped Tasks</h1>
+          <ProgressBar />
+        </header>
+        <main>
+          <Accordion />
+        </main>
+      </div>
     </div>
-  </div>
+  </GroupsContextProvider>
 );
 
 export default App;
---
+++ src/contexts/GroupsContext.tsx
@@ -0,0 +1,38 @@
+import { IGroupData } from 'interfaces/groups';
+import {
+  createContext,
+  Dispatch,
+  FC,
+  ReactNode,
+  SetStateAction,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
+import { fetchUsersGroups } from 'services/groups.service';
+import { getArrayWithId } from 'utils/formatData';
+
+interface IContextData {
+  usersGroups: IGroupData[];
+  setUsersGroups: Dispatch<SetStateAction<IGroupData[]>>;
+}
+
+export const GroupsContext = createContext<IContextData>({
+  usersGroups: [],
+  setUsersGroups: () => {},
+});
+
+export const GroupsContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const [usersGroups, setUsersGroups] = useState<IGroupData[]>([]);
+
+  useEffect(() => {
+    fetchUsersGroups().then((groups) => {
+      const formattedGroups = getArrayWithId(groups);
+      setUsersGroups(formattedGroups);
+    });
+  }, []);
+
+  const memoizedGroups = useMemo(() => ({ usersGroups, setUsersGroups }), [usersGroups]);
+
+  return <GroupsContext.Provider value={memoizedGroups}>{children}</GroupsContext.Provider>;
+};
